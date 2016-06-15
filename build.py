dfc63fbccb763d67a47f80d6f8553d005c2c97ee
Daniel Clarke <u8sand@gmail.com>
2016-06-15 14:57:49 -0400

    Vastly simplified things by using a sqlite3 database
    - Join becomes a lot easier
    - Altering the way the text looks can be done as views, create view_out to get loaded into the webpage

--- build.py
+++ build.py
@@ -1,7 +1,7 @@
 #!/usr/local/bin/python3
 
 import os
-import pandas as pd
+import sqlite3
 from distutils.dir_util import copy_tree
 from staticjinja import make_site
 
@@ -12,48 +12,15 @@ def register(func):
 	filters[func.__name__] = func
 
 @register
-def pd_to_table(df):
-	return df.to_dict(orient='split')
-
-@register
-def pd_select(df, attr, val):
-	return df[df[attr] == val]
-
-@register
-def pd_apply_format(df, attr, fmt):
-	df[attr] = df[attr].apply(fmt.format)
-	return df
-
-@register
-def trim_(data):
-	i = data.rfind('_')
-	return data[:i] if i!=-1 else data
+def header(table):
+	return [desc[0] for desc in table.description]
 
 def context(template):
-	components = pd.read_csv(data_root+'components.csv', index_col=False)
-	interactions = pd.read_csv(data_root+'interactions.csv', index_col=False)
-
-	# Create interaction table
-
-	# prepare columns
-	interaction_components = components[['Official Symbol', 'Swiss-Prot ID', 'Functional Category', 'FA']]
-	source_components = interaction_components.rename(columns={col: '%s_source' % (col) for col in components.columns})
-	target_components = interaction_components.rename(columns={col: '%s_target' % (col) for col in components.columns})
-	# merge components into interactions
-	interactions_joined = pd.merge(
-		pd.merge(interactions, source_components,
-			left_on='Source', right_on='Official Symbol_source', how='inner'), target_components,
-			left_on='Target', right_on='Official Symbol_target', how='inner', suffixes=('_','')).drop(
-				['Official Symbol_source', 'Official Symbol_target'], axis=1)
-	# rearrange columns
-	source_cols = [col for col in interactions_joined.columns if col.endswith('_source')]
-	target_cols = [col for col in interactions_joined.columns if col.endswith('_target')]
-	cols 		= [col for col in interactions_joined.columns if col not in source_cols and col not in target_cols and col not in ['Source', 'Target']]
-	interactions_joined = interactions_joined[['Source']+source_cols+['Target']+target_cols+cols]
+	con = sqlite3.connect(data_root+'db.sqlite3')
 
 	return {
-		'components': components,
-		'interactions': interactions_joined,
+		'components': con.cursor().execute('select * from components_out'),
+		'interactions': con.cursor().execute('select * from interactions_out'),
 	}
 
 site = make_site(
