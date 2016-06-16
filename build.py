ce46c82f2be8fb1105bae62cdf68a0b723b8461d
Daniel Clarke <u8sand@gmail.com>
2016-06-16 10:30:27 -0400

    Using Clustergrammer js resources to display custergrams
    - Got ./build.py to generate the association pages from the datasets in the database
    - Fixed up _associations page to handle drawing the clustergram from the dataset data field
    - Updating processing to use clustergrammer and generate relevant datasets for display
    - Database holds viz, sim_row, and sim_col ready for the association page to display
    - All dependencies of clustergrammer were brought in under static/clustergrammer

--- build.py
+++ build.py
@@ -5,37 +5,64 @@ import sqlite3
 from distutils.dir_util import copy_tree
 from staticjinja import make_site
 
+# Establish connection to database
 con = sqlite3.connect('data/db.sqlite3')
 
-funcs={}
-def register_func(func):
-	funcs[func.__name__] = func
+# Utility functions
+def try_ignore(stmt):
+	try:
+		exec(stmt)
+	except:
+		pass
 
+def trim_(s):
+	i = s.rfind('_')
+	if i != -1:
+		return s[:i]
+	else:
+		return s
+
+# Jinja Filters
 filters={}
 def register_filter(func):
 	filters[func.__name__] = func
 
 @register_filter
 def header(table):
-	return [desc[0] for desc in table.description]
+	return [trim_(desc[0]) for desc in table.description]
+
+@register_filter
+def first(table):
+	return table.fetchone()
+
+# Jinja Functions
+funcs={}
+def register_func(func):
+	funcs[func.__name__] = func
 
 @register_func
 def query(stmt, *kargs):
 	return con.cursor().execute(stmt, kargs)
 
-def associations(env, template, **kwargs):
-	env.get_template('_association.html').stream(**kwargs).dump(template)
+# Remove old build directory
+try_ignore(r"os.rmdir('build/')")
 
+# Create build directory tree
+try_ignore(r"os.makedirs('build/associations/')")
+
+# Copy static files to build
+copy_tree('static/', 'build/')
+
+# Build static pages
 site = make_site(
 	searchpath='templates/',
 	filters=filters,
 	contexts=[('.*', funcs)],
 	outpath='build')
-
-# TODO:
-# for ID in funcs['query']('select `ID` from `datasets`'):
-# 	site.render_template('associations/%d.html' % (ID), dict(ID=ID), '_association.html')
-
 site.render()
 
-copy_tree('static/', 'build/')
+# Build subpages from database
+for name, in funcs['query']('select `Name` from `datasets`'):
+	page = 'build/associations/%s.html' % (name.lower().replace(' ', '_'))
+	site.get_template('_association.html').stream(name=name, **funcs).dump(page)
+	print('Rendering %s' % (page))
--- process/process_matrix.py
+++ process/process_matrix.py
@@ -1,10 +1,15 @@
-#!/usr/local/bin/python3
+#!/usr/local/bin/python
+# Note: python2 because clustergrammer is python2
 
 import os
 import sys
 import json
 import sqlite3
 import pandas as pd
+
+# Location of clustergrammer
+sys.path.insert(0, '../clustergrammer/')
+sys.path.insert(0, '..')
 
 # Load database
 con = sqlite3.connect('../data/db.sqlite3')
@@ -34,11 +39,22 @@ for arg in sys.argv[1:]:
 				'val': float(val),
 				'row_name': str(ind),
 			} for ind, val in zip(col.index, col)]
-		} for col_name, col in gene_attribute_matrix.to_dict(orient='series').items()],
+		} for col_name, col in gene_attribute_matrix.to_dict(orient='series').iteritems()],
 	}
 
+	# Custergrammer
+	from clustergrammer import Network
+	net = Network()
+	net.load_vect_post_to_net(vector_json)
+	net.make_clust(dist_type='cos',views=['N_row_sum', 'N_row_var'], dendro=True,
+				   sim_mat=True, filter_sim=0.1, calc_cat_pval=False)
+
 	# Insert into database
-	cur.execute('insert into `datasets` (`Name`, `Clustergrammer`) values (?, ?)', (name, json.dumps(vector_json)))
+	cur.execute('insert into `datasets` (`Name`, `viz`, `sim_row`, `sim_col`) values (?, ?, ?, ?)',
+		(name,
+		 net.export_net_json('viz', indent='no-indent'),
+		 net.export_net_json('sim_row', indent='no-indent'),
+		 net.export_net_json('sim_col', indent='no-indent')))
 
 con.commit()
 con.close()
