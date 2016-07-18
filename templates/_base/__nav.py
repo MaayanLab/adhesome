4c564fe0ee50608373cc001e42d6bcebd58b81e2
Daniel Clarke <u8sand@gmail.com>
2016-07-18 15:31:34 -0400

    Association submenu visibility fixed

--- templates/_base/__nav.py
+++ templates/_base/__nav.py
@@ -1,9 +1,9 @@
 (
 	("Home", ("home", "index.html", [])),
-	("Components", ("components", "components.html", {{ evaluate("components/__nav.py", subpage=subpage) }})),
+	("Components", ("components", "components.html", {{ evaluate("components/__nav.py") }})),
 	("Interactions", ("interactions", "interactions.html", [])),
-	("Associations", ("associations", "associations.html", {{ evaluate("associations/__nav.py", subpage=subpage) }})),
-	("Predictions", ("predictions", "predictions.html", {{ evaluate("predictions/__nav.py", subpage=subpage) }})),
+	("Associations", ("associations", "associations.html", {{ evaluate("associations/__nav.py", subpage=subpage, uri=uri) }})),
+	("Predictions", ("predictions", "predictions.html", {{ evaluate("predictions/__nav.py") }})),
 	("Downloads", ("downloads", "downloads.html", [])),
 	("Contact", ("contact", "contact.html", [])),
 )
